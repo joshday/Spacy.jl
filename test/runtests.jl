8d2fa48b1fecc42066639dac90ced8026532459e
Josh Day <emailjoshday@gmail.com>
2022-06-16 09:19:33 -0400

    basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using Spacy
+using Conda
+using PyCall
 using Test
 
 @testset "Spacy.jl" begin
-    # Write your tests here.
+    @test "spacy" in Conda._installed_packages()
+    @test spacy isa PyObject
 end
